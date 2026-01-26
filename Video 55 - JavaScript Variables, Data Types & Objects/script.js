c0f25c82ac81e538426df869e5495758e01840f2
SAURAV SARWAN <sauravsarwan1234@gmail.com>
2026-01-26 14:22:06 +0530

    Video 54 done

---
+++ Video 55 - JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,43 @@
+
+// let is blocked scoped
+// var is globally scoped
+
+console.log("Hey this is tutorial 55")
+
+let a = 5;
+// a = a + 1;
+let b = 6;
+let c = "Harry";
+let _a = "Shivam"
+
+// console.log(a + b + 8)
+// console.log(typeof a,typeof b, typeof c );
+{
+    // let a = 66;  
+    console.log(a)
+}
+
+console.log(a);
+
+
+let x = "Saurav bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    "name": "Harry",
+    "job role": 5600,
+    // "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
