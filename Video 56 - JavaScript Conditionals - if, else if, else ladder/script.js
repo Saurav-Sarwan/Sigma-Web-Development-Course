811494071bf2ad4ba9466f7e4da73a3b85861d4a
SAURAV SARWAN <sauravsarwan1234@gmail.com>
2026-01-26 14:47:25 +0530

    Video 56 done

---
+++ Video 56 - JavaScript Conditionals - if, else if, else ladder/script.js
@@ -0,0 +1,23 @@
+console.log("Hello I am conditional tutorial")
+
+let age = 18;
+// let grace = 2;
+
+// age += grace;
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+if(age > 18){
+    console.log("You Cannot Drive")
+}
+else if(age == 0){
+    console.log("Are You Kidding?")
+}
+else{
+    console.log("You Can Drive")
+}
