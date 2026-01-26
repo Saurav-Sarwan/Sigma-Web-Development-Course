814b6853e4e1923502449a672c9a4f0a68bd6a0e
SAURAV SARWAN <sauravsarwan1234@gmail.com>
2026-01-26 15:27:15 +0530

    Video 57 done

---
+++ Video 57 - JavaScript Loops/script.js
@@ -0,0 +1,39 @@
+console.log("Hey i am tutorial on loops")
+
+// let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);    
+// }
+
+// let obj = {
+//     name : "Saurav",
+//     role : "Engineering",
+//     Department : "EiE"
+// }
+
+// for(const key in obj){
+//     // console.log(key)
+//     const element = obj[key]
+//     console.log(key,element)
+// }
+
+// for (const c of "Saurav") {
+//     console.log(c)
+// }
+
+
+// let i = 1
+// while(i<6){
+//     console.log(i)
+//     i++
+// }
+
+let i = 10;
+do{
+    console.log(i)
+    i++
+}while(i<6)
